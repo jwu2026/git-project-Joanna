08ff945fa08b307dfe056335dfac2630d3bab038
Joanna <jwu2@hwemail.com>
2025-11-06 12:54:30 -0800

    (GP-2.1): Program Can Initialize Repository
    Added a method to initialize a Git repo

--- Git.java
+++ Git.java
@@ -1,5 +1,25 @@
-public class Git {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
 
+public class Git {
+    public static void main(String[] args) throws IOException {
+        newRepo();
+    }
+
+    public static void newRepo() throws IOException {
+        File git = new File("git");
+        File objects = new File("git/objects");
+        File index = new File("git/index");
+        File HEAD = new File("git/HEAD");
+
+        if (git.exists() && objects.exists() && index.exists() && HEAD.exists()) {
+            git.mkdir();
+            objects.mkdir();
+            index.createNewFile();
+            HEAD.createNewFile();
+            System.out.println("Git Repository Already Exists");
+        } else {
+            System.out.println("Git Repository Created");
+        }
     }
 }
